c9bf2c2409dede10f95ddf55acfa3a4d6874c34a
Ignatenko Timofey <itv@gmprint.ru>
2017-07-04 13:00:38 +0300

    styles,scripts,docs

--- src/js/tableHoverPickObj.js
+++ src/js/tableHoverPickObj.js
@@ -1,5 +1,6 @@
 (function() {
     $(document).ready(function() {
+        // Init all tables this class "table-pick"
         var tables = $(".table-pick");
         /** Object**/
         var Table = {
@@ -8,6 +9,10 @@
             trs: null,
             tds: null,
 
+            /**
+             * wrap text in td
+             * 
+             */
             spanText: function() {
                 for (var i = 0, j = $(this.tds).length; i < j; ++i) {
                     var text = $((this.tds)[i]).text(); // text 
@@ -15,9 +20,14 @@
                 }
             },
 
+            /**
+             * Leave table
+             * 
+             */
             tableLeave: function() {
                 $(this.obj).mouseleave(function() {
 
+                    console.log("LEAVE ")
                     $(this.tds).find('span').removeClass("pick");
                     $(this.tds).parent().removeClass("hoverRow");
                     $(this).find('span').removeClass("hoverCol");
@@ -31,27 +41,23 @@
         for (var i = 0; i < tables.length; i++) {
             Table.index = [i];
             Table.obj = tables[i];
-
-
             Table.trs = $(Table.obj).find("tr");
             Table.tds = $(Table.obj).find("td");
             console.log("tds.length - " + i + " " + Table.tds.length);
             console.log("trs.length - " + i + " " + Table.trs.length);
-            Table.spanText();
-            Table.tableLeave();
-
-
-            tdsOver(Table.tds, Table.trs);
-            tdsOut(Table.tds, Table.trs);
-            // for (var j = 0; j<Table.tds.length;j++)
-            //   {
-            //     tdsOver(Table.tds, Table.trs);
-            //     tdsOut(Table.tds,Table.trs);
-            //   }
-            // tdsOver(Table.tds);
-            // tdsOut(Table.tds); 
+            Table.spanText(); // wrap span 
+            Table.tableLeave(); // leave table method
+            tdsOver(Table.tds, Table.trs); // handler Over
+            tdsOut(Table.tds, Table.trs); // handler Out
         }
 
+        /**
+         * hover cols
+         * 
+         * @param {any} elem 
+         * @param {any} trs 
+         * @returns 
+         */
         function colHover(elem, trs) {
             var itcol = $(elem).parent().children().index($(elem));
             var $hovCol = $(trs).find('td:eq(' + itcol + ')').find('span');
@@ -59,8 +65,14 @@
 
         }
 
+        /**
+         * handler Over
+         * 
+         * @param {any} tds 
+         * @param {any} trs 
+         */
         function tdsOver(tds, trs) {
-            $(tds).on('mouseover', function() {
+            $(tds).on('mouseenter', function() {
                 console.log("over");
                 resetHover(tds, trs);
                 $(this).find('span').addClass("pick");
@@ -72,9 +84,16 @@
 
         }
 
+        /**
+         * handler Over
+         * 
+         * @param {any} tds 
+         * @param {any} trs 
+         */
         function tdsOut(tds, trs) {
-            $(tds).on('mouseout', function() {
+            $(tds).on('mouseleave', function() {
                 console.log("out");
+                resetHover(tds, trs);
                 $(this).find('span').removeClass("pick");
                 $(this).parent().removeClass('hoverRow');
                 $(colHover(this, trs)).removeClass("hoverCol");
@@ -83,11 +102,26 @@
 
         }
 
+        /**
+         * reset all Hover
+         * 
+         * @param {any} tds 
+         * @param {any} trs 
+         */
         function resetHover(tds, trs) {
             $(tds).find('span').removeClass("pick");
             $(tds).find('span').removeClass("hoverCol");
             $(trs).removeClass("hoverRow");
         }
+
+
+        // /***Click reset */
+
+
+        // $(document).on('click', function(event) {
+        //     if (event.target.id != "tables")
+        //         alert("e.target.id -- " + event.currentTarget);
+        // });
 
 
 
